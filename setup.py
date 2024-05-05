f9c46336394783aad6bedbb0f539b822762a1982
Farhan Shaikh <thinkingdatascience@gmail.com>
2024-05-05 06:46:16 +0300

    setup added

--- setup.py
+++ setup.py
@@ -1,29 +1,9 @@
-import setuptools
+from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
-
-
-__version__ = "0.0.0"
-
-REPO_NAME = "Industry-Safety"
-AUTHOR_USER_NAME = "thinkingdatascience"
-SRC_REPO = "ISD"
-AUTHOR_EMAIL = "thinkingdatascience@gmail.com"
-
-
-setuptools.setup(
-    name=SRC_REPO,
-    version=__version__,
-    author=AUTHOR_USER_NAME,
-    author_email=AUTHOR_EMAIL,
-    description="A small python package for CNN app",
-    long_description=long_description,
-    long_description_content="text/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    project_urls={
-        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
-    },
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+setup(
+    name="ISD",
+    version="0.0.0",
+    author="Farhan Shaikh",
+    author_email="thinkingdatascience@gmail.com",
+    packages=find_packages(),
 )
